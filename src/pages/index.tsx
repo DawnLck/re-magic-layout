03b0dbc1e44a239cdd81d1bba3ede122696d7e66
LiangChaokai <youchoudeyu@qq.com>
2021-03-15 08:07:39 +0800

    🎨 refactor: 引入栅格系统，主页切换成三栏布局

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,18 @@ import './index.less';
 
 import { MockCard } from '../widgets';
 
+import { Row, Col } from 'antd';
+
 export default function IndexPage() {
   return (
-    <div className="page">
-      <div className="control"></div>
-      <div className="container">
-        <MockCard width={400} height={250}></MockCard>
-      </div>
-    </div>
+    <Row className="page" gutter={{ xs: 8, sm: 16, md: 24 }}>
+      <Col flex="300px" className="settings"></Col>
+      <Col flex="auto" className="main">
+        <div className="control"></div>
+        <div className="container">
+          <MockCard width={400} height={250}></MockCard>
+        </div>
+      </Col>
+    </Row>
   );
 }
