a417065586267514d4a76837756843347f0cce15
Dawnlck <youchoudeyu@qq.com>
2021-07-19 22:13:27 +0800

    👷 chore(dev): 更新本地开发配置

--- .umirc.ts
+++ .umirc.ts
@@ -4,7 +4,9 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [{ path: '/', component: '@/pages/index' }],
+  devServer: {
+    port: 9027,
+  },
   // alias: { umi 自带了两个有用的alias，我觉得足够了，后面还不够的话可以再补充进来
   //   '~@src': 'src',
   //   '~@widgets': 'src/widgets',
